1b5921778d0e5b21bde8ec4a062e0d07ec7e8d2a
kazuya-ide <166674572+kazuya-ide@users.noreply.github.com>
2024-12-21 03:53:37 +0900

    全体のモバイルせってい見直し

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,14 +17,8 @@
    children: React.ReactNode
  }) {
    return (
-     <html lang="en" className="bg-white dark:bg-black">
-       <head>
-       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-       </head>
-       <body className={inter.className}>
+     <html lang="en">
       
-           {children}
-         </body>
      </html>
    )
  }
